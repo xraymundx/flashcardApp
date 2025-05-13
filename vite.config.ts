41b9b522fe53c0df94f86384af1533dd0c7c0545
xraymundx <raymudmacapundag113@gmail.com>
2025-05-13 08:08:33 +0800

    added file to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/flashcardApp/', // Set this to your repo name for GitHub Pages
   plugins: [
     react(),
     legacy()
